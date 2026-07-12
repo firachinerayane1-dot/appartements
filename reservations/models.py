994d79e2bf09273e8c2a45348dae8c6a70b876ca
Iliane <ilianefirachine0@gmail.com>
2026-07-13 00:00:24 +0100

    ajouter le model de reservation dans resrvations/models.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Reservation(models.Model):
+    STATUT=[
+        ("ANNULEE","annulée"),
+        ("EN_ATTENTE","en attente")
+        ("CONFIRMEE","confirmée"),
+        ("TERMINEE","Terminée"),
+        ("REJETEE","Rejetée"),
+    ]
+    TYPE = [
+        ("STANDARD", "Standard"),
+        ("REGULIER", "Client régulier"),
+    ]
+    #une reservation lier a un Utilisateur
+    utilisateur = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.PROTECT,related_name="reservations")
+    appartement = models.ForeignKey("apartments.appartement",on_delete=models.PROTECT,related_name="reservations")
+    date_reservation = models.DateField(auto_now_add=True)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    statut=models.CharField(choices=STATUT)
+    remise_appliquee=models.DecimalField(max_digits=10,decimal_places=2)
+    prix_total = models.DecimalField(max_digits=10, decimal_places=2)
+    type_reservation= models.CharField(choices=TYPE)
+
+    def __str__(self):
+        return f"{self.utilisateur} - {self.appartement} - {self.date_reservation}"
